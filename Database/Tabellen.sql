9987117dd9d8808f9c3769d9506d1e06bbf479a7
Joep Hoffland <joephoffland@gmail.com>
2019-03-19 14:52:40 +0100

    ADD database informatie

---
+++ Database/Tabellen.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS
+  PlaylistTrack, Tracks, Playlists, Users;
+
+CREATE TABLE Users (
+  username  VARCHAR(50)   NOT NULL,
+  password  VARCHAR(250)  NOT NULL,
+  CONSTRAINT  pk_username PRIMARY KEY (username)
+);
+
+CREATE TABLE Playlists (
+  id    INT           NOT NULL  AUTO_INCREMENT,
+  name  VARCHAR(50)   NOT NULL,
+  owner VARCHAR(50)   NOT NULL,
+  CONSTRAINT  pk_playlistId     PRIMARY KEY (id),
+  CONSTRAINT  fk_playlistOwner  FOREIGN KEY (owner)
+              REFERENCES  Users(username)
+              ON DELETE   RESTRICT
+              ON UPDATE   CASCADE
+);
+
+CREATE TABLE Tracks (
+  id              INT           NOT NULL  AUTO_INCREMENT,
+  performer       VARCHAR(250)  NOT NULL,
+  title           VARCHAR(250)  NOT NULL,
+  url             BLOB          NOT NULL,
+  duration        INT           NOT NULL,
+  album           VARCHAR(250)  NULL,     /* only for SONG  */
+  publicationDate DATE          NULL,     /* only for VIDEO */
+  description     TEXT          NULL,     /* only for VIDEO */
+  CONSTRAINT  pk_trackId  PRIMARY KEY (id)
+);
+
+CREATE TABLE PlaylistTrack (
+  playlist          INT     NOT NULL,
+  track             INT     NOT NULL,
+  offlineAvailable  BOOLEAN NOT NULL  DEFAULT FALSE,
+  CONSTRAINT  fk_playlist FOREIGN KEY (playlist)
+              REFERENCES  Playlists(id)
+              ON DELETE   RESTRICT
+              ON UPDATE   CASCADE,
+  CONSTRAINT  fk_track    FOREIGN KEY (track)
+              REFERENCES  Tracks(id)
+              ON DELETE   RESTRICT
+              ON UPDATE   CASCADE
+);
